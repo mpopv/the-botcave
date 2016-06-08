27b7b8ed989c374eedc5b146d7b02099e8826441
Matt Popovich <mattpopovich@outlook.com>
2016-06-07 21:23:17 -0400

    Building word banks

--- cyber.js
+++ cyber.js
@@ -3,12 +3,6 @@
 
  var Twit = require('twit');
  var TwitterBot = require('node-twitterbot').TwitterBot;
- var title = ''; var subtitle = ''; var styles = ''; var selectedStyle = '';
- var artcArray = [ ]; var nounArray = [ ]; var plurArray = [ ];
- var actnArray = [ ]; var verbArray = [ ]; var adjeArray = [ ];
- var maybArray = [ ]; var thngArray = [ ];
- var dice = [ 1, 2, 3 ]; var roll = '';
-
 
  var Bot = new TwitterBot({
    consumer_key:         process.env.CYBERBOT_CONSUMER_KEY,
@@ -17,7 +11,8 @@
    access_token_secret:  process.env.CYBERBOT_ACCESS_TOKEN_SECRET
  });
 
- ///////////////// Choose Random Element in a Array ////////////////////////////
+
+///////////////// Choose Random Element in a Array ////////////////////////////
 
  function chooseRandom(myArray) {
    return myArray[Math.floor(Math.random() * myArray.length)];
@@ -29,27 +24,45 @@ var jobSuff;
 var jobTtlS;
 var corps;
 
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////// Jobs & Titles ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
 // Job Prefixes that can be attached as a compound word
 jobPreN =   [ 'net', 'neuro', 'techno', 'psyko', 'anarcho', 'hover', 'bio',
-              'solo', 'med', 'data', 'cyber', 'arch', 'under', 'sewer'
+              'solo', 'med', 'data', 'cyber', 'arch', 'under', 'sewer', 'hack'
+              'deep', 'nano', 'street', 'ex-', 'gene'
             ];
 
 // Job prefixes that can be adjectives modifying a title
 jobPreS =   [ 'corporate', 'data', 'cyborg', 'grand', 'low', 'virtual',
-              'hacker'
+              'hacker', 'dread', 'digital', 'street', 'sector'
             ];
 
 // Job suffixes
 jobSuff =   [ 'mancer', 'runner', 'techie', 'tech', 'terrorist', 'separatist',
               'anarchist', 'hacker', 'junkie', 'courier', 'merc', 'panzer',
-              'boy', 'girl', 'nerd', 'surfer', 'cyborg', 'fixer', 'nomad'
+              'boy', 'girl', 'nerd', 'surfer', 'cyborg', 'fixer', 'nomad',
+              'punk', 'cultist', 'slinger', 'doctor', [ 'thief', 'thieves' ],
+              'mage', 'bandit', 'technician', 'shade', 'trader',
+              'smuggler', 'keeper', 'medic', 'rat', 'splicer', 'slicer'
             ];
 
 // Job titles that can be independent w/ adjective modifier
 jobTtlS =   [ 'enforcer', 'scientist', 'psyker', 'anarch', 'fixer', 'hacker',
               'junkie', 'courier', 'agent', 'nomad', 'prince',
-              [ 'princess', 'princesses' ]
+              [ 'princess', 'princesses' ], [ 'ronin', 'ronin' ],
+              [ 'samurai', 'samurai' ], 'blackhat', 'whitehat', 'cultist',
+              'rebel', 'juggernaut', 'technician', 'trader', 'smuggler',
+              'medic', 'drifter', 'splicer', 'slicer'
             ];
+
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////// Jobs & Titles ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 // Corporations
 corps =     [ 'Infocomp', 'IEC', 'EBM', 'Biotechnica', 'Arasaka',
