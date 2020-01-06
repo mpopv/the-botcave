df24bf16d3f6dd738bfc45d9e87ae2232f0dbd3a
Matt Popovich <mattpopovich@outlook.com>
2020-01-05 23:47:02 -0800

    cyber: fixing typo in cyber bot test file

--- src/megacitypolice/test.js
+++ src/megacitypolice/test.js
@@ -2,5 +2,5 @@ const { oneIn } = require("../utils");
 const generateCyberText = require("./generate-text");
 const generateCyberAd = require("./generate-advertisement");
 
-let generateText = oneIn(1) ? generateCyberAd : generateCyberText;
+let generateText = oneIn(10) ? generateCyberAd : generateCyberText;
 generateText();
