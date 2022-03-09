6bec0b51bb3972cc765e3111d61327c5f0130622
mpopv <mattpopovich@outlook.com>
2022-03-09 11:22:25 -1000

    retry requests

--- ukraine.js
+++ ukraine.js
@@ -10,6 +10,18 @@ const accountUrl = `https://twitter.com/ukrainemapbot`;
 const mapUrl = `https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/2022_Russian_invasion_of_Ukraine.svg/2199px-2022_Russian_invasion_of_Ukraine.svg.png`;
 const chromeUAString = `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`;
 
+const retry = async (fn, retryDelay = 100, numRetries = 3) => {
+    for (let i = 0; i < numRetries; i++) {
+      try {
+        return await fn();
+      } catch (e) {
+        if (i === numRetries - 1) throw e;
+        await delay(retryDelay);
+        retryDelay = retryDelay * 2;
+      }
+    }
+};
+
 const client = new TwitterApi({
     appKey: process.env.UKRAINEMAPBOT_API_KEY,
     appSecret: process.env.UKRAINEMAPBOT_API_KEY_SECRET,
@@ -25,14 +37,14 @@ const rwClient = client.readWrite;
 
   // Get latest map + description
   const page = await context.newPage();
-  await page.goto(url);
+  await retry(() => await page.goto(url), 1000, 10);
   await page.waitForLoadState('networkidle');
   const lastMapDescription = await page.locator('.filehistory > tbody > tr:nth-child(2) td:nth-child(6)').innerText();
   console.log(lastMapDescription);
 
   // Get latest tweet
   const account = await context.newPage();
-  await account.goto(accountUrl);
+  await retry(() => account.goto(accountUrl), 1000, 10);
   await account.waitForLoadState('networkidle');
   const tweetTextEl = await account.locator('[data-testid="tweet"] >> nth=0').innerText();
   console.log(tweetTextEl);
